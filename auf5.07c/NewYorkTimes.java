e97e66453c8bc205276b68c7eac77e693908b4db
André Marín <andre.m@game-matter.com>
2025-10-08 12:21:47 +0200

    Update NewYorkTimes.java

--- auf5.07c/NewYorkTimes.java
+++ auf5.07c/NewYorkTimes.java
@@ -9,13 +9,11 @@ public class NewYorkTimes
       System.out.println("");
       
       for(int m = 8; m <= 11; m++)
-      
       {
          System.out.println("Reaching the " + m + "th avenue");
 
          for(int i = 14; i <= 20; i = i + 2)
          {
-
             System.out.println("Enter No " + i);
          
             for(int n = 2; n <= 5; n++)
