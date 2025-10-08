bc19abcacf34958f14fdd86b56f44aad51752429
Marc Anton <marcanton.berschneider@gmail.com>
2025-10-08 11:17:21 +0200

    Create Hochhaus2.java

---
+++ auf5.07/Hochhaus2.java
@@ -0,0 +1,31 @@
+// u05_kontrollstrukturen
+// auf5.07
+
+public class Hochhaus2
+{
+   public static void main (String[] args)
+   { 
+      
+      System.out.println("        /\\");
+      System.out.println("       /  \\");
+      System.out.println("      /    \\");
+      System.out.println("     /      \\");
+      System.out.println("    /        \\");
+      System.out.println("   /   (  )   \\");
+      System.out.println("  /            \\");
+      System.out.println(" /______________\\");
+      System.out.println("/|              |\\");
+      
+      for(int i = 1; i <= 4; i++)
+      {
+         System.out.println(" | ___      ___ |");
+         System.out.println(" | | |      | | |");
+         System.out.println(" | ---      --- |");
+      }
+         System.out.println(" | Hotel    ___ |");
+         System.out.println(" | |   |    | | |");
+         System.out.println(" | |   |    --- |");
+         System.out.println("==================");
+   }
+                          
+}
