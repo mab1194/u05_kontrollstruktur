f39aed9800849548938fc6301d70c8526fae7cff
Marc Anton <marcanton.berschneider@gmail.com>
2025-10-08 12:19:56 +0200

    Fix NewYorkTimes.java

--- auf5.07c/NewYorkTimes.java
+++ auf5.07c/NewYorkTimes.java
@@ -8,17 +8,17 @@ public class NewYorkTimes
       System.out.println("Reaching the 5th avenue");
       System.out.println("");
       
-      for(int m = 5; m <= 7; m++)
+      for(int m = 8; m <= 11; m++)
       
       {
          System.out.println("Reaching the " + m + "th avenue");
 
-         for(int i = 21; i <= 25; i = i + 2)
+         for(int i = 14; i <= 20; i = i + 2)
          {
 
             System.out.println("Enter No " + i);
          
-            for(int n = 0; n <= 2; n++)
+            for(int n = 2; n <= 5; n++)
             {
               System.out.println("Delivered in No " + i + ", "+ "floor " + n);
             }
