07f3fb3853215e48b853a1a6384617fa58d76bfe
Marc Anton <marcanton.berschneider@gmail.com>
2025-10-22 15:57:22 +0200

    Fix code format from ArabischeZahlen.java

--- auf5.45/ArabischeZahlen.java
+++ auf5.45/ArabischeZahlen.java
@@ -6,7 +6,7 @@ public class ArabischeZahlen
    {
       // Deklarationen
       int arabischeZahl;
-      String roemischeZahl;
+      String roemischeZahl = "Roemische Zahl: ";
 
       // Eingabe einlesen
       InputStreamReader eingabeLeser      = new InputStreamReader(System.in);
@@ -16,76 +16,77 @@ public class ArabischeZahlen
       String eingabe = gepufferterLeser.readLine();
       arabischeZahl  = Integer.parseInt(eingabe);
          
-      while(arabischeZahl>0)
+      while(arabischeZahl > 0)
       {
-         if((arabischeZahl>=1000))
+         if((arabischeZahl >= 1000))
          {
             arabischeZahl = arabischeZahl - 1000;
            
-            System.out.print("M");
+            roemischeZahl = roemischeZahl + "M";
          }
-         else if (arabischeZahl<1000 && arabischeZahl>=900)
+         else if (arabischeZahl < 1000 && arabischeZahl >= 900)
          {
             arabischeZahl = arabischeZahl - 900;
            
-            System.out.print("CM"); 
+            roemischeZahl = roemischeZahl + "CM"; 
          }         
-         else if(arabischeZahl>=500)
+         else if(arabischeZahl >= 500)
          {
             arabischeZahl = arabischeZahl - 500;
-            System.out.print("D"); 
+            roemischeZahl = roemischeZahl + "D"; 
          }
-         else if(arabischeZahl>=400 && arabischeZahl<500)
+         else if(arabischeZahl >= 400 && arabischeZahl < 500)
          {
             arabischeZahl = arabischeZahl - 400;
-            System.out.print("CD");        
+            roemischeZahl = roemischeZahl + "CD";        
          }
-         else if(arabischeZahl>=100)
+         else if(arabischeZahl >= 100)
          {
             arabischeZahl = arabischeZahl - 100;
-            System.out.print("C"); 
+            roemischeZahl = roemischeZahl + "C"; 
          }
-         else if(arabischeZahl >=90 && arabischeZahl<100)
+         else if(arabischeZahl >= 90 && arabischeZahl < 100)
          {
             arabischeZahl = arabischeZahl - 90;
-            System.out.print("XC");
+            roemischeZahl = roemischeZahl + "XC";
          }        
-         else if(arabischeZahl>=50)
+         else if(arabischeZahl >= 50)
          {
             arabischeZahl = arabischeZahl - 50;
-            System.out.print("L"); 
+            roemischeZahl = roemischeZahl + "L"; 
          }
-         else if(arabischeZahl>=40 && arabischeZahl<50)
+         else if(arabischeZahl >= 40 && arabischeZahl < 50)
          {
             arabischeZahl = arabischeZahl - 40;
-            System.out.print("XL");
+            roemischeZahl = roemischeZahl + "XL";
          }
-         else if(arabischeZahl>=10)
+         else if(arabischeZahl >= 10)
          {
             arabischeZahl = arabischeZahl - 10;
-            System.out.print("X"); 
+            roemischeZahl = roemischeZahl + "X"; 
          }
-         else if(arabischeZahl>=9 && arabischeZahl<10)
+         else if(arabischeZahl >= 9 && arabischeZahl < 10)
          {
             arabischeZahl = arabischeZahl - 9;
-            System.out.print("IX");
+            roemischeZahl = roemischeZahl + "IX";
          }
-         else if(arabischeZahl>=5)
+         else if(arabischeZahl >= 5)
          {
             arabischeZahl = arabischeZahl - 5;
-            System.out.print("V"); 
+            roemischeZahl = roemischeZahl + "V"; 
          }
-         else if(arabischeZahl>=4 && arabischeZahl<5)
+         else if(arabischeZahl >= 4 && arabischeZahl < 5)
          {
             arabischeZahl = arabischeZahl - 4;
-            System.out.print("IV");
+            roemischeZahl = roemischeZahl + "IV";
          }
-         else if(arabischeZahl>0 && arabischeZahl<4)
+         else if(arabischeZahl > 0 && arabischeZahl < 4)
          {
             arabischeZahl = arabischeZahl - 1;
-            System.out.print("I"); 
+            roemischeZahl = roemischeZahl + "I"; 
          }
       }
+      System.out.println(roemischeZahl);
       System.out.print("\n");
    }   
 }
